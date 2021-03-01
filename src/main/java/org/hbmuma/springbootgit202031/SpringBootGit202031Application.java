73ea9e1950a413f8bb166643a1a74e8745bc2daa
Willem-Kmetsch <1600511929@qq.com>
2021-03-01 14:50:33 +0800

    init project

---
+++ src/main/java/org/hbmuma/springbootgit202031/SpringBootGit202031Application.java
@@ -0,0 +1,13 @@
+package org.hbmuma.springbootgit202031;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGit202031Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGit202031Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/hbmuma/springbootgit202031/SpringBootGit202031ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hbmuma.springbootgit202031;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGit202031ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
