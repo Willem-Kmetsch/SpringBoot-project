d8f2bd3d2c35122396a0d8ce43489a8c0b0be1f2
Willem-Kmetsch <1600511929@qq.com>
2021-03-02 13:34:57 +0800

    init project

---
+++ src/main/java/org/hbmuma/springbootgit202031/controller/MyController.java
@@ -0,0 +1,32 @@
+package org.hbmuma.springbootgit202031.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Parameter;
+import java.util.Map;
+
+@Controller
+public class MyController {
+    // 到登陆页面
+    @RequestMapping("/")
+    public String toLogin(){
+        return "login";
+    }
+    // 登陆
+    @RequestMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password,
+                        Map<String, String> map, HttpSession session){
+        if(!StringUtils.isEmpty(username) && "123".equals(password)){
+            session.setAttribute("user",username);
+            return "index";
+        } else{
+            map.put("msg","用户名密码错误");
+            return "login";
+        }
+    }
+}
